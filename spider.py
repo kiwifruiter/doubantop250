294d042c0eaf598a6f6c009885edc3170411c9b8
7 <loverooster@163.com>
2020-09-15 00:06:01 +0800

    解析数据

--- spider.py
+++ spider.py
@@ -1,4 +1,4 @@
-import urllib.request,urllib.error
+import urllib.request, urllib.error
 import re
 import bs4
 
@@ -10,16 +10,29 @@ def main():
     savepath = ".\\豆瓣电影Top250.xls"
     # 3.保存数据
     # saveData(savepath)
-    askURL("https://movie.douban.com/top250?start=0")
+    # askURL("https://movie.douban.com/top250?start=0")
 
 
 # if __name__ == "__main__":
 #     main()
+findLink = re.compile(r'<a href="(.*?)">')
+
 
 # 爬取网页
 def getData(baseurl):
     datalist = []
-    # 2.逐一解析数据
+    for i in range(0, 1):
+        url = baseurl + str(i * 25)
+        html = askURL(url)
+        # 2.逐一解析数据
+        soup = bs4.BeautifulSoup(html, "html.parser")
+        for item in soup.find_all('div', class_='item'):
+            # print(item) 查看电影item全部信息
+            data = []
+            item = str(item)
+            link = re.findall(findLink, item)[0]
+            print(link)
+
     return datalist
 
 
@@ -42,7 +55,7 @@ def askURL(url):
         if hasattr(e, "reason"):
             print((e.reason))
 
-    # return html
+    return html
 
 
 # 保存数据
---
+++ test/testBs4.py
@@ -0,0 +1,6 @@
+#BS将HTML文档转换成一个复杂的树形结构
+
+
+from bs4 import BeautifulSoup
+
+t_list= bs.find_all(id)
